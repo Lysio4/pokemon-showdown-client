a109bbdbc08257f5bfe1803a3e1341635234a6cd
Lysio4 <lukas.moncorge@proton.me>
2025-11-21 11:36:33 +0100

    trying some stuff

--- play.pokemonshowdown.com/src/battle-dex-search.ts
+++ play.pokemonshowdown.com/src/battle-dex-search.ts
@@ -1170,10 +1170,10 @@ class BattlePokemonSearch extends BattleTypedSearch<'pokemon'> {
 			tierSet = tierSet.slice(slices.Regular);
 		// AGE + Touhoumons
 		} else if (format === 'gen9agoldenexperience') tierSet = tierSet.slice(slices.OU, slices.AG || slices.Uber).concat(tierSet.slice(slices.UU));
-		else if (format === 'gen9agoldenexperienceuu' || format.includes('agoldenexperienceuu')) tierSet = tierSet.slice(slices.UU);
+		/*else if (format === 'gen9agoldenexperienceuu' || format.includes('agoldenexperienceuu')) tierSet = tierSet.slice(slices.UU);
 		else if (format === 'gen9agoldenexperienceru' || format.includes('agoldenexperienceru')) tierSet = tierSet.slice(slices.RU);
 		else if (format === 'gen9agoldenexperiencenu' || format.includes('agoldenexperiencenu')) tierSet = tierSet.slice(slices.NU);
-		else if (format === 'gen9agoldenexperiencepu' || format.includes('agoldenexperiencepu')) tierSet = tierSet.slice(slices.PU);
+		else if (format === 'gen9agoldenexperiencepu' || format.includes('agoldenexperiencepu')) tierSet = tierSet.slice(slices.PU);*/
 		else if (format === 'toho' || format.includes('toho') || format === 'touhoumons' || format.includes('touhoumons')) {
 			tierSet = tierSet.slice(slices.Toho, slices.AG);
 		} else if (!isDoublesOrBS) {
