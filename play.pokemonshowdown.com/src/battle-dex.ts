2f312bdfcbce191f3225099eb2a67a61d6479b0b
Lysio4 <lukas.moncorge@proton.me>
2025-11-23 19:05:12 +0100

    I think I have the icons

--- play.pokemonshowdown.com/src/battle-dex.ts
+++ play.pokemonshowdown.com/src/battle-dex.ts
@@ -229,6 +229,9 @@ export const Dex = new class implements ModdedDex {
 
 	pokeballs: string[] | null = null;
 
+	//TODO we might want to move this to something like data/petmods
+	readonly modResourcePrefix = 'https://raw.githubusercontent.com/scoopapa/dh2/master/data/mods/';
+
 	/*resourcePrefix = (() => {
 		let prefix = '';
 		if (window.document?.location?.protocol !== 'http:') prefix = 'https:';
