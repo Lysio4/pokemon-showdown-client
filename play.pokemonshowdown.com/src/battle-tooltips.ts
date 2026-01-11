0cf8817b5126eba6f6c356ac4235ff329c78a2a2
Lysio4 <lukas.moncorge@proton.me>
2026-01-11 11:28:59 +0100

    tryna fix shit

--- play.pokemonshowdown.com/src/battle-tooltips.ts
+++ play.pokemonshowdown.com/src/battle-tooltips.ts
@@ -2183,6 +2183,7 @@ export class BattleTooltips {
 				value.abilityModify(this.battle.gen > 6 ? 1.2 : 1.3, "Galvanize");
 				value.abilityModify(this.battle.gen > 6 ? 1.2 : 1.3, "Pixilate");
 				value.abilityModify(this.battle.gen > 6 ? 1.2 : 1.3, "Refrigerate");
+				value.abilityModify(this.battle.gen > 6 ? 1.2 : 1.3, "Ignite");
 			}
 			if (this.battle.gen > 6) {
 				value.abilityModify(1.2, "Normalize");
