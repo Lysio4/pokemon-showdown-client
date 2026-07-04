0ec02540efb035fb16aa334f37422286702d0742
Lysio4 <lukas.moncorge@proton.me>
2026-07-04 14:38:05 +0200

    doubles and co

--- play.pokemonshowdown.com/src/battle-dex-search.ts
+++ play.pokemonshowdown.com/src/battle-dex-search.ts
@@ -946,8 +946,9 @@ abstract class BattleTypedSearch<T extends SearchType> {
 			if (this.formatType === 'frlg') table = table['gen3frlg'];
 			if (this.formatType === 'champions') table = table['champions'];
 			if (this.formatType === 'natdexchampions') table = table['natdexchampions'];
-			if (this.formatType === 'agoldenexperience') table = table['gen9agoldenexperience'];
-			if (this.formatType === 'agoldenexperiencedoubles') table = table['gen9agoldenexperiencedoubles'];
+			if (this.formatType === 'agoldenexperience' && !this.isDoubles) table = table['gen9agoldenexperience'];
+			if (this.formatType === 'agoldenexperience' && this.isDoubles) table = table['gen9agoldenexperiencedoubles'];
+			// if (this.formatType === 'agoldenexperiencedoubles') table = table['gen9agoldenexperiencedoubles'];
 			if (this.formatType === 'toho') table = table['gen9toho'];
 			let learnset = table.learnsets[learnsetid];
 			const eggMovesOnly = this.eggMovesOnly(learnsetid, speciesid);
