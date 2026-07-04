5b3d7b77dc0845585eb5144c21ae73b7b20d0294
Lysio4 <lukas.moncorge@proton.me>
2026-07-04 15:26:46 +0200

    doubles and co

--- play.pokemonshowdown.com/src/battle-dex-search.ts
+++ play.pokemonshowdown.com/src/battle-dex-search.ts
@@ -1168,7 +1168,7 @@ class BattlePokemonSearch extends BattleTypedSearch<'pokemon'> {
 		let slices: { [k: string]: number } = table.formatSlices;
 		if (
 			format === 'ubers' || format === 'uber' || format === 'ubersuu' ||
-			format === '4v4doublesuu' || format === 'nationaldexdoubles'*
+			format === '4v4doublesuu' || format === 'nationaldexdoubles'
 		) {
 			tierSet = tierSet.slice(slices.Uber);
 		} else if (isVGCOrBS || (isHackmons && dex.gen === 9 && !this.formatType)) {
